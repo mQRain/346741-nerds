17536e38a35e8930fcc2bf47f9a882343981b6a0
Olga <wunden@yandex.ru>
2017-02-18 18:06:56 +1000

    анимация модального окна и интерактивная карта

---
+++ js/feedback.js
@@ -0,0 +1,38 @@
+var link = document.querySelector(".address .btn");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-feedback-close");
+
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[name=email]");
+var message = popup.querySelector("[name=message]");
+
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.add("modal-feedback-show");
+    username.focus();
+});
+
+close.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.remove("modal-feedback-show");
+    popup.classList.remove("modal-feedback-error");
+});
+
+form.addEventListener("submit", function (event) {
+    if (!username.value || !email.value || !message.value) {
+        event.preventDefault();
+        popup.classList.remove("modal-feedback-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-feedback-error");
+    }
+});
+
+window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-feedback-show")) {
+            popup.classList.remove("modal-feedback-show");
+            popup.classList.remove("modal-feedback-error");
+        }
+    }
+});
